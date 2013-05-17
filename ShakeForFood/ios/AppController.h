334f81ad17602d92a3205b189aac6127e4869297
DavidYang <weflytotti@qq.com>
2013-05-17 13:49:44 +0800

    Initial Commit

---
+++ ShakeForFood/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  ShakeForFoodAppController.h
+//  ShakeForFood
+//
+//  Created by Yang David on 5/17/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController	*viewController;
+}
+
+@end
+
